b9cce50cd2b78be97157e60d24fafe46d405c23a
najwanan <theunnamedrd@gmail.com>
2022-03-13 23:52:45 -0700

    repeat string solution added

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function (str, n) {
+    
+    let newStr = ""
+    if (n < 0) {
+        return error;
+        
+    } else {
+         for (let i = 0; i < n; i++) {
+           newStr = newStr + str;
+         }
+         return newStr;
+        
+    }
+   
+    console.log(newStr);
 };
 
 // Do not edit below this line
